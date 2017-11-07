23589b2606624959e4a87467507bcd90dc2da7d8
Lilian Shih <l.shih@outlook.com>
2017-11-07 02:00:40 +0000

    linted

--- download_avatars.js
+++ download_avatars.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-const isRequired = () => {throw error('parameter is required'); };
 const repoOwner = process.argv[2];
 const repoName = process.argv[3];
 const checkerOwner = process.argv.slice(2);
@@ -15,7 +14,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
     console.log("You forgot either the username or the repo name. Try again.");
   } else {
     const options = {
-      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
       headers: {
         'User-Agent': 'request',
         'Authorization': "token " + secret.GITHUB_TOKEN
@@ -25,22 +24,20 @@ function getRepoContributors(repoOwner, repoName, cb) {
       const obj = JSON.parse(body);
       cb(obj);
     });
-  };
+  }
 }
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
-       .on('error', function (err) {
-         throw err;
-       })
-       .on('response', function (response) {
-       })
-       .pipe(fs.createWriteStream(filePath))
+    .on('error', function (err) {
+      throw err;
+    })
+    .pipe(fs.createWriteStream(filePath));
 }
 
 getRepoContributors(repoOwner, repoName, (obj) => {
   obj.forEach((function(obj) {
-    downloadImageByURL(obj["avatar_url"], "avatars/"+obj["login"]+".jpg");
+    downloadImageByURL(obj["avatar_url"], `avatars/${obj["login"]}.jpg`);
   }));
   console.log('Avatars have been downloaded. Check the avatars folder!');
 });
